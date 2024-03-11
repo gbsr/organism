94c01e8bc8c8daf13d8fbb09290452852c0a06f9
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2024-03-11 15:44:49 +0100

    added canvas-card + main setup for classes

---
+++ script.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+console.log(context);
+
+
+class Particle {
+
+}
+
+class Effect {
+
+}
+
+function animate() {
+
+}
+
+
