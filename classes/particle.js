47b63cdc2ea5b1ffb887071813b3e865bb0b35d8
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2024-03-15 02:10:40 +0100

    added cohesion rule to boid imp

--- classes/particle.js
+++ classes/particle.js
@@ -20,8 +20,6 @@ class Particle {
 		this.repel = isRepellingMouse;
 		this.attract = isAttractedToMouse;
 
-
-
 		/**
 		 * Generate random x and y coordinates within the bounds of the effect, offset
 		 * by the particle size. This positions particles randomly within the effect area.
@@ -30,8 +28,8 @@ class Particle {
 		this.y = this.size + Math.random() * (this.effect.height - this.size * 2);
 
 		// Define the minimum and maximum velocity
-		const minVelocity = 0.02; // Adjust this value as needed
-		const maxVelocity = 0.5; // Adjust this value as needed
+		const minVelocity = 0.0002; // Adjust this value as needed
+		const maxVelocity = 0.0005; // Adjust this value as needed
 
 		// Initialize the velocities
 		this.vx = Math.random() * (maxVelocity - minVelocity) + minVelocity;
@@ -42,7 +40,6 @@ class Particle {
 		if (Math.random() < 0.5) this.vy *= -1;
 
 	}
-
 
 	/**
 	 * Draws the particle on the canvas by creating a path and arc
@@ -56,25 +53,51 @@ class Particle {
 		context.fillStyle = this.color;
 	}
 
-
-
 	update() {
 
 		this.x += this.vx;
 		this.y += this.vy;
 
-		// // Bounce off edges
-		if (this.x > this.effect.width - this.size || this.x < this.size) {
+		// To make sure we bounce off the edge, and also stay within the canvas at all times
+		this.HandleEdgeDetection();
+
+		// boids implementation
+
+		// cohesion
+		for (let i = 0; i < this.effect.particles.length; i++) {
+			let other = this.effect.particles[i];
+			let distance = Math.hypot(this.x - other.x, this.y - other.y);
+			if (distance < 100 && other !== this) {
+
+				let dx = this.x - other.x;
+				let dy = this.y - other.y;
+				let angle = Math.atan2(dy, dx);
+				this.vx -= Math.cos(angle) * 0.0005;
+				this.vy -= Math.sin(angle) * 0.0005;
+			}
+		}
+		// alignment
+		// separation
+
+	}
+
+
+	HandleEdgeDetection() {
+		const buffer = 20;
+
+		// Bounce off edges
+		if (this.x > this.effect.width - this.size - buffer || this.x < this.size + buffer) {
 			this.vx *= -1;
 		}
 
-		if (this.y > this.effect.height - this.size || this.y < this.size) {
+		if (this.y > this.effect.height - this.size - buffer || this.y < this.size + buffer) {
 			this.vy *= -1;
 		}
-		this.x = Math.max(Math.min(this.x, this.effect.width - this.size), this.size);
-		this.y = Math.max(Math.min(this.y, this.effect.height - this.size), this.size);
-	}
 
+		// Ensure position stays within bounds
+		this.x = Math.max(Math.min(this.x, this.effect.width - this.size - buffer), this.size + buffer);
+		this.y = Math.max(Math.min(this.y, this.effect.height - this.size - buffer), this.size + buffer);
+	}
 }
 
 export default Particle;
